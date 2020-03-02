8931f7801b57f70a360457e800b4a450f72d128f
Justin Johnson <justin.d.johnson@gmail.com>
2020-03-02 13:54:44 -0700

    Updated json parser to use pandoc and string cleaning

--- Toolchest/FaqJsonParser.cs
+++ Toolchest/FaqJsonParser.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using Toolchest.Extensions;
 using Toolchest.Logging;
 using Toolchest.Models;
 
@@ -61,6 +63,7 @@ namespace Toolchest
 
             foreach(var parent in items)
             {
+                Log.Instance.Debug($"Processing parent: {parent.Id}");
                 var dir = Path.Combine(outputDirectory, parent.Id);
                 if(!Directory.Exists(dir))
                 {
@@ -83,25 +86,46 @@ namespace Toolchest
 
                 foreach (var child in parent.RuntimeChildren)
                 {
+                    Log.Instance.Debug($"\tProcessing child: {child.Id}");
                     var childFilename = $"{child.Id}.md";
+                    var htmlFilename = Path.Combine(dir, $"{child.Id}.html");
                     var childPath = Path.Combine(dir, childFilename);
                     var childSb = new StringBuilder();
+                    string answerMd = "";
 
+                    // convert HTML -> MD with pandoc (must be installed to path!)
+                    File.WriteAllText(htmlFilename, child.Ans);
+                    using (var pandoc = new ProcessWrapper("pandoc", $"-t markdown -o {childPath} {htmlFilename} --wrap=none"))
+                    {
+                        if(pandoc.Run())
+                        {
+                            // nothing here intentionally
+                        }
+                        else
+                        {
+                            throw new Exception($"Pandoc conversion failed: " + pandoc.Error.ToString());
+                        }
+                    }
+                    answerMd = File.ReadAllText(childPath);
+                    File.Delete(htmlFilename);
+
+                    // strip any remaining HTML
+                    answerMd = Regex.Replace(answerMd, "<[^>]+>", "");
+
+                    // build the markdown file
                     var childMeta = new PageMeta
                     {
-                        Title = child.Faq,
-                        Description = "",
+                        Title = child.Faq.TruncateTo(140),
+                        Description = child.Ans.TruncateTo(140),
                         Date = DateTime.Now,
                         MsAuthor = this.MsAuthor,
                         Author = this.Author,
                         AssetId = Guid.NewGuid().ToString("D")
                     };
-
-                    // build the markdown file
                     var childMetaString = yamlSerializer.Serialize(childMeta);
                     childSb.Append($"---\n{childMetaString}---\n\n");
                     childSb.Append($"## {child.Faq}\n\n");
-                    childSb.Append($"{child.Ans}\n");
+                    childSb.Append($"{answerMd}\n");
                     File.WriteAllText(childPath, childSb.ToString());
 
                     // include file in parent
